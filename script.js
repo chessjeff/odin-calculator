e4327b8937555a44214702d1aee53b95a91d45e5
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-10-11 15:42:29 -0500

    Defines expression keys with button presses

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 let expression = {
-    //a: num,
-    //op: str,
-    //b: num,
+    a: [],
+    op: '',
+    b: [],
 }
 
-function operate(expression) {
+//each number listens and adds itself via writeNumbers
+const nums = Array.from(document.getElementsByClassName('num'));
+nums.forEach((num) => {
+    num.addEventListener('click', () => writeNumbers(num.id))
+});
+
+//when equals is pushed insert: expression.a = expression.a.join(expression.a, '');
+// and expression.b = expression.b.join(b, '');
+
+//creates array to be converted to string by operating function
+function writeNumbers(num) {
+    if (!expression.op) {
+        expression.a.push(num);
+    } else {
+        expression.b.push(num); 
+    }
+}
+
+//when op is added, writeNumbers adds to b
+//can only take one op and only after a is true
+const operators = Array.from(document.getElementsByClassName('operator'))
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        if (expression.a && !expression.op) {
+            expression.op = operator.textContent
+        };
+    })
+})
+
+//pressing equals calls operate()
+
+
+function chooseOperator() {
     switch (expression.op) {
         case '+':
             add(expression.a, expression.b);
@@ -35,4 +67,3 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-
