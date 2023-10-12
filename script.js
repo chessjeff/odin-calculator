dea32799268d4d6ac730535e53542b3f49d0d85a
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-10-11 20:49:43 -0500

    Cleans up js and adds a few features

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ let expression = {
     b: [],
 }
 
+const display = document.querySelector('.display')
+
 //each number listens and adds itself via writeNumbers
 const nums = Array.from(document.getElementsByClassName('num'));
 nums.forEach((num) => {
-    num.addEventListener('click', () => writeNumbers(num.id));
+    num.addEventListener('click', () => {
+        writeExpression(num.id);
+    });
 });
 
 //can only take one op and only after a is true
@@ -16,11 +20,12 @@ operators.forEach((operator) => {
     operator.addEventListener('click', () => {
         if (expression.a.length > 0 && !expression.op) {
             expression.op = operator.textContent;
+            writeExpression();
         };
     })
 })
 
-//pressing equals executes expression
+//pressing equals executes expression only after op and b are chosen
 const equals = Array.from(document.getElementsByClassName('equals'));
 equals.forEach((button) => {
     button.addEventListener('click', () => {
@@ -30,49 +35,88 @@ equals.forEach((button) => {
     })
 });
 
-//creates arrays to be converted to string by execute function
-function writeNumbers(num) {
-    //writes a first and waits for operator selection to write b
-    if (!expression.op) {
-        expression.a.push(num);
-    } else {
-        expression.b.push(num); 
-    }
+//clears display div and expression
+const clear = Array.from(document.getElementsByClassName('clear'));
+clear.forEach((button) => {
+    button.addEventListener('click', () => {
+        display.textContent = '0';
+        clearExpression();
+    })
+})
 
+function clearExpression() {
+    expression = {
+        a: [],
+        op: '',
+        b: [],
+    }
+}
+
+//requires 'a' to be selected, then an operator, then 'b'
+function writeExpression(num) {
+    let text = display.textContent;
+    //first button press expression.a will be empty
+    //after check if no operator
+    if (expression.a.length < 1 || !expression.op) {
+        if (!expression.op) {
+            expression.a.push(num);
+            a = parseInt(expression.a.join(expression.a, ''));
+            display.textContent = a;
+        }
+    //make sure an operator is selected
+    } else if (!(text.includes('+') || text.includes('-') || text.includes('*') || text.includes('/'))) {
+        display.textContent = a + ' ' + expression.op;
+    //b logic similar to a
+    } else {
+        expression.b.push(num);
+        b = parseInt(expression.b.join(expression.b, ''));
+        display.textContent = a + ' ' + expression.op + ' ' + b;
+    }
 }
 
 function execute() {
     a = parseInt(expression.a.join(expression.a, ''));
     b = parseInt(expression.b.join(expression.b, ''));
-    const result = chooseOperator(a, b);
-    console.log(result);
-}
-
-function chooseOperator(a, b) {
-    switch (expression.op) {
-        case '+':
-            return add(a, b);
-        case '-':
-            return subtract(a, b);
-        case '*':
-            return multiply(a, b);
-        case '/':
-            return divide(a, b);
+    
+    //lets continuous operations be done
+    a = operate(a, b);
+    display.textContent = a;
+    expression = {
+        a: Array.from(a.toString()).map(Number),
+        op: '',
+        b: [],
     }
 }
 
-function add(a, b) {
-    return a + b;
+function operate(a, b) {
+    switch (expression.op) {
+        case '+':
+            return operations.add(a, b);
+        case '-':
+            return operations.subtract(a, b);
+        case '*':
+            return operations.multiply(a, b);
+        case '/':
+            if (b == 0) {
+                clearExpression();
+                alert("DON'T DO THAT");
+            } else {
+                return operations.divide(a, b);
+            }
+    }
 }
 
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    return a / b;
+const operations = {
+    add(a, b) {
+        return a + b;
+    },
+    subtract(a, b) {
+        return a - b;
+    },
+    multiply(a, b) {
+        return a * b;
+    },
+    divide(a, b) {
+        return a / b;
+    }
 }
